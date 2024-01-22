3000faf1379ae1336d8c1474ef96b0510f3ca20c
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-01-21 17:14:59 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Author: Magaly Rodriguez
+ * Course: COMP-003A
+ * Purpose: Code for assignment 1
+*/
+
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. I like to paint");
+            Console.WriteLine("2. I like to use acrylics");
+            Console.WriteLine("3. I like to use watercolors");
+            Console.WriteLine("4. I like to use oil paints");
+            Console.WriteLine("5. I like to play video games");
+            Console.WriteLine("6. I like to play Roblox");
+            Console.WriteLine("7. I like to play the Sims 3");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Author: Magaly Rodriguez
+ * Course: COMP-003A
+ * Purpose: Code for assignment 1
+*/
+
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. I like to paint");
+            Console.WriteLine("2. I like to draw on paper and pencil");
+            Console.WriteLine("3. I like to draw on my iPad");
+            Console.WriteLine("4. I own 5 cats");
+            Console.WriteLine("5. My favorite cat is Paca");
+            Console.WriteLine("6. My favorite colors are orange and yellow");
+            Console.WriteLine("7. My hair is brown");
+            Console.WriteLine("8. My eyes are brown");
+        }
+    }
+}
