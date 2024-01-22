c95125e2d4db2a0e4946504d764e5ecc607e939b
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-01-21 17:23:14 -0800

    Added 25 things about me

--- Program.cs
+++ Program.cs
@@ -19,6 +19,23 @@ namespace COMP003A.Assignment1
             Console.WriteLine("6. My favorite colors are orange and yellow");
             Console.WriteLine("7. My hair is brown");
             Console.WriteLine("8. My eyes are brown");
+            Console.WriteLine("9. I was born in the United States");
+            Console.WriteLine("10. My favorite food is sushi");
+            Console.WriteLine("11. I love the rain");
+            Console.WriteLine("12. I would love to visit New Zealand");
+            Console.WriteLine("13. I would love to live in New Zealand one day");
+            Console.WriteLine("14. I hate spiders");
+            Console.WriteLine("15. I am 23 years old");
+            Console.WriteLine("16. I am the middle child");
+            Console.WriteLine("17. I am the only female between my siblings");
+            Console.WriteLine("18. I am taller than my grandma");
+            Console.WriteLine("19. I like to sing");
+            Console.WriteLine("20. My favorite music genre is 90s RnB");
+            Console.WriteLine("21. My favorite artist is Mariah Carey");
+            Console.WriteLine("22. My favorite song from Mariah Carey is Honey");
+            Console.WriteLine("23. I love the song Around the Way Girl by LL Cool J");
+            Console.WriteLine("24. My parents were born in the 70s");
+            Console.WriteLine("25. I love Kool and the Gang");
         }
     }
 }
