eacfa242d56c4ada16fa86741a80fb0ed972a59a
magalyrodriguez1 <142467680+magalyrodriguez1@users.noreply.github.com>
2024-01-21 17:55:04 -0800

    added 50 things about me

--- Program.cs
+++ Program.cs
@@ -36,6 +36,32 @@ namespace COMP003A.Assignment1
             Console.WriteLine("23. I love the song Around the Way Girl by LL Cool J");
             Console.WriteLine("24. My parents were born in the 70s");
             Console.WriteLine("25. I love Kool and the Gang");
+            Console.WriteLine("26. I love the song Too Hot by Kool and the Gang");
+            Console.WriteLine("27. I like to sing while I am driving");
+            Console.WriteLine("28. I wish I had the money to live on my own");
+            Console.WriteLine("29. I work as a tutor at an elementary school");
+            Console.WriteLine("30. I hope to not work in education next year");
+            Console.WriteLine("31. I will try to get an apprenticeship sometime this year");
+            Console.WriteLine("32. I know HTML and CSS but I do not know JavaScript yet");
+            Console.WriteLine("33. I was a computer science major when I attended Fresno State");
+            Console.WriteLine("34. I once knew a little bit of C++ but forgot basically everything now");
+            Console.WriteLine("35. Introduction to data structures made me change my major");
+            Console.WriteLine("36. I changed my major to art during my third year of college at Fresno State");
+            Console.WriteLine("37. I graduated from Fresno State in 2022 and I loved the art program there");
+            Console.WriteLine("38. I would love to go back for my master's in Art one day when I have the money to");
+            Console.WriteLine("39. I am working on my second degree right now");
+            Console.WriteLine("40. I am majoring in Information Technology with the full stack web development track");
+            Console.WriteLine("41. I had a hard time choosing between UX design track and full stack web development");
+            Console.WriteLine("42. I decided to give coding another chance despite struggling with it at Fresno State");
+            Console.WriteLine("43. I am really enjoying the program here at West Hills");
+            Console.WriteLine("44. I have attended West Hills when I was in HS. I had been to Coalinga and Firebaugh campus");
+            Console.WriteLine("45. I refuse to drive on freeways. I prefer to take regular streets");
+            Console.WriteLine("46. I was born on October 25th, 2000.");
+            Console.WriteLine("47. I don't know if I will try for my bachelor's in CS again because I don't have the money for it");
+            Console.WriteLine("48. I have lived in the same house for nearly my whole life. My dad got lucky");
+            Console.WriteLine("49. I used to be a dog person up until I was 12");
+            Console.WriteLine("50. I used to live in an apartment when I was in my first 2 years at Fresno State before COVID");
+            Console.WriteLine()
         }
     }
 }
